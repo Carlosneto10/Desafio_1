a9bce15a708d17b65a9c3682835d876f8eb9331e
Carlosneto10 <carloseperez96@gmail.com>
2023-03-14 21:08:48 -0600

    prueba

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+using namespace std;
+#define PI 3.141592
 int main(int argc, char** argv) {
-	return 0;
+    int exit;
+    cout << PI << endl;
+    cin >> exit;
+    cout << exit << endl;
+    return 0;
 }
